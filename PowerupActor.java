d5b6e12a3350cd06289a56218339e71304178406
Caroollll <349351197@gapps.yrdsb.ca>
2024-01-16 08:46:45 -0500

    Update PowerupActor.java

--- PowerupActor.java
+++ PowerupActor.java
@@ -1,10 +1,9 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class PowerupActor here.
+ * powerup actor for tutorial page
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @carol
  */
 public class PowerupActor extends Actor
 {
